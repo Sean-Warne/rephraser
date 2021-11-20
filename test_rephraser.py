8e6471e10cc9e86788eafe0ba8abe6b68bd3ab17
Sean Warne <Sean-Warne@users.noreply.github.com>
2021-11-20 12:54:00 -0700

    Tweaked so that word will not be input until it hits a retry max (#1)

--- test_rephraser.py
+++ test_rephraser.py
@@ -3,6 +3,7 @@ import rephraser
 
 class test_rephraser(unittest.TestCase):
     def test_rephrase_string(self):
-        rephrased_str = rephraser.rephrase_string('Happy cat')
+        phrase = "don't touch me"
+        rephrased_str = rephraser.rephrase_string(phrase)
         print(rephrased_str)
-        self.assertEqual(type(rephrased_str), type('Happy cat'))
+        self.assertEqual(type(rephrased_str), type(phrase))
