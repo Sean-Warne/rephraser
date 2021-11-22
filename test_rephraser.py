4044629e8618abbee5d2df67b551412bbc447d3b
Sean Warne <sean.andrew.warne@gmail.com>
2021-11-21 20:25:57 -0700

    Fixed output return

--- test_rephraser.py
+++ test_rephraser.py
@@ -4,6 +4,6 @@ import rephraser
 class test_rephraser(unittest.TestCase):
     def test_rephrase_string(self):
         phrase = "happy cat"
-        rephrased_str = rephraser.rephrase_string(in_str=phrase, out_style='title')
+        rephrased_str = rephraser.rephrase_string(in_str=phrase, out_style='username')
         print(rephrased_str)
         self.assertEqual(type(rephrased_str), type(phrase))
