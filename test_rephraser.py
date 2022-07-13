7c93931d1bb03ddcf20fa3c8eeac11d7734b10f2
Sean Warne <sean.andrew.warne@gmail.com>
2022-07-12 20:33:29 -0600

    Formatting, fixed borked loop, and added new word size param

--- test_rephraser.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import rephraser
-
-class test_rephraser(unittest.TestCase):
-    def test_rephrase_string(self):
-        phrase = "happy cat"
-        rephrased_str = rephraser.rephrase_string(in_str=phrase, out_style='username')
-        print(rephrased_str)
-        self.assertEqual(type(rephrased_str), type(phrase))
